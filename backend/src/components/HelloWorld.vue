51527b434b1c5370da907de8d732f3986ac16ac2
Hanifak47 <hanifsmurf@gmail.com>
2025-08-07 14:39:32 +0700

    +install vuex dan buat store +install vue router dan buat routenya

--- backend/src/components/HelloWorld.vue
+++ backend/src/components/HelloWorld.vue
@@ -1,15 +1,20 @@
 <script setup>
-import { ref } from 'vue'
+import { ref, computed } from "vue";
+import store from "../store";
+
+const test = computed(() => store.state.test);
 
 defineProps({
   msg: String,
-})
+});
 
-const count = ref(0)
+const count = ref(0);
 </script>
 
 <template>
-  <h1 class ="bg-purple-300">{{ msg }}</h1>
+  <h1 class="bg-purple-300">{{ msg }}</h1>
+
+  {{ test }}
 
   <div class="card">
     <button type="button" @click="count++">count is {{ count }}</button>
@@ -21,15 +26,12 @@ const count = ref(0)
 
   <p>
     Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-      >create-vue</a
+    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank">create-vue</a
     >, the official Vue + Vite starter
   </p>
   <p>
     Learn more about IDE Support for Vue in the
-    <a
-      href="https://vuejs.org/guide/scaling-up/tooling.html#ide-support"
-      target="_blank"
+    <a href="https://vuejs.org/guide/scaling-up/tooling.html#ide-support" target="_blank"
       >Vue Docs Scaling up Guide</a
     >.
   </p>
